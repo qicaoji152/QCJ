aad63943f560ea3988098d51a734f8ed4aef235c
qicaoji152 <qicaoji15219260891@qq.com>
2026-03-27 21:28:40 +0800

    加入base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ import { defineUserConfig } from 'vuepress'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
+  base: "/QCJ/",
   lang: 'en-US',
 
   title: 'VuePress',
